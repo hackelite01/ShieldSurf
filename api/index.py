b2f4959128e3b255eae7b2315c9a8197b681cfcf
Mayank Rajput (HackElite) <hackelite.sup@gmail.com>
2025-10-06 21:02:06 +0530

    Fix working directory for Vercel runtime

--- api/index.py
+++ api/index.py
@@ -1,9 +1,12 @@
 import sys
 import os
 
-# Add parent directory to path so we can import app
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+# Add parent directory to path so we can import modules
+parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, parent_dir)
 
+# Change working directory to parent so relative paths work
+os.chdir(parent_dir)
+
+# Import the Flask app
 from app import app
-
-# This is what Vercel will use
